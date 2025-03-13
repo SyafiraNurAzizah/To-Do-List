0222e80d39d499b4e010959cab37197d76e18fd6
Syafira Nur Azizah <syafiranurazizah1710@gmail.com>
2025-03-13 08:51:14 +0700

    perbaikan tampilan registrasi

--- registrasi.php
+++ registrasi.php
@@ -9,24 +9,29 @@
 </head>
 <body>
     <div class="container">
-        <h1>Sign Up</h1>
-        <p>Register to your account</p>
+        <h2>Sign Up</h2>
+        <!-- <p>Register to your account</p> -->
         <form action="proses/proses_register.php" method="POST">
-            <label>Username</label> <br>
-            <input type="text" name="username" id="username" required> <br>
+            <div class="left">
+                <label>Full Name</label> <br>
+                <input type="text" name="name" id="name" placeholder="Enter your full name" required> <br>
 
-            <label>Password</label> <br>
-            <input type="password" name="password" id="password" required> <br>
+                <label>E-Mail</label> <br>
+                <input type="text" name="email" id="email" placeholder="Enter your e-mail" required> <br>
 
-            <label>Nama Lengkap</label> <br>
-            <input type="text" name="name" id="name" required> <br>
+                <label>Birth Date</label> <br>
+                <input type="date" name="birth_date" id="birth_date" required> <br>
+            </div>
 
-            <label>E-Mail</label> <br>
-            <input type="text" name="email" id="email" required> <br>
+            <div class="right">
+                <label>Username</label> <br>
+                <input type="text" name="username" id="username" placeholder="Enter your username" required> <br>
 
-            <label>Tanggal Lahir</label> <br>
-            <input type="date" name="birth_date" id="birth_date" required> <br>
-            <button type="submit">Sign Up</button>
+                <label>Password</label> <br>
+                <input type="password" name="password" id="password" placeholder="Enter your password" required> <br>
+
+                <button type="submit">Sign Up</button>
+            </div>
         </form>
         <p class="sign-in">Already have an account? <b><a href="login.php">Log In here</a></b>.</p>
     </div>
